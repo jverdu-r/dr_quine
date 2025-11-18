b290d044e34a8692655fcc25b94b5a46e1a714a2
jverdu-r <jorge.verdu.ruiz@gmail.com>
2025-11-18 15:32:29 +0100

    assembly Grace working now fixing it compile with nasm

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,206 +1,57 @@
-%define FILENAME "Grace_kid.s"
-%define FILEMODE "w"
-%define CONTENT "%cdefine FILENAME %c%s%c%c%cdefine FILEMODE %c%s%c%c%cdefine CONTENT %c%s%c%c"
+; One Comment
+BITS 64
 
-; Grace assembly quine
 section .data
-filename db FILENAME, 0
-mode db FILEMODE, 0
-data db CONTENT, 0
+    model db "; One Comment%1$cBITS 64%1$c%1$csection .data%1$c    model db %2$c%3$s%2$c, 0%1$c    filename db %2$cGrace_kid.s%2$c, 0%1$c    mode_w db %2$cw%2$c, 0%1$c%1$c%1$csection .text%1$c    global main%1$c    extern fopen%1$c    extern fclose%1$c    extern fprintf%1$c%1$c%4$cmacro FT 0%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    push rbx%1$c%1$c    open_file:%1$c        lea rdi, [rel filename]%1$c        lea rsi, [rel mode_w]%1$c        call fopen wrt ..plt%1$c%1$c        test rax, rax%1$c        jz .open_failed%1$c        mov rbx, rax%1$c        jmp write_in_file%1$c    %1$c    .open_failed:%1$c        mov eax, 1%1$c        jmp exit_process%1$c%1$c    write_in_file:%1$c        mov rdi, rbx%1$c        lea rsi, [rel model]%1$c        mov rdx, 10%1$c        mov rcx, 34%1$c        lea r8, [rel model]%1$c        mov r9, 37%1$c        xor rax, rax%1$c        call fprintf wrt ..plt%1$c%1$c    close_file:%1$c        mov rdi, rbx%1$c        call fclose wrt ..plt%1$c    %1$c    exit_process:%1$c        pop rbx%1$c        pop rbp%1$c        xor eax, eax%1$c        ret%1$c%4$cendmacro%1$c%1$cFT%1$c", 0
+    filename db "Grace_kid.s", 0
+    mode_w db "w", 0
 
-; Array of pointers to format strings
-parts_array:
-    dq part1, part2, part3, part4, part5, part6, part7, part8
-    dq part9, part10, part11, part12, part13, part14, part15, part16
-    dq part17, part18
-parts_count equ ($ - parts_array) / 8
-
-; Format strings for different parts of the source
-part1 db "%cdefine FILENAME %c%s%c%c", 0          ; First macro
-part2 db "%cdefine FILEMODE %c%s%c%c", 0          ; Second macro  
-part3 db "%cdefine CONTENT %c%s%c%c%c", 0         ; Third macro
-part4 db "; Grace assembly quine%c", 0             ; Comment
-part5 db "section .data%c", 0                     ; Data section header
-part6 db "filename db FILENAME, 0%c", 0           ; Data declarations
-part7 db "mode db FILEMODE, 0%c", 0
-part8 db "data db CONTENT, 0%c%c", 0              ; Data + blank line
-part9 db "section .text%c", 0                     ; Text section header
-part10 db "    global main%c", 0                  ; Global declaration
-part11 db "    extern fopen, fprintf, fclose%c%c", 0  ; Extern + blank line
-part12 db "main:%c", 0                            ; Main label
-part13 db "    push rbp%c    mov rbp, rsp%c    push r12%c", 0  ; Function prologue
-part14 db "    lea rdi, [rel filename]%c    lea rsi, [rel mode]%c", 0
-part15 db "    call fopen wrt ..plt%c    mov r12, rax%c", 0
-part16 db "    ; Write source code using loop%c", 0
-part17 db "    mov rdi, r12%c    call fclose wrt ..plt%c", 0
-part18 db "    pop r12%c    pop rbp%c    xor eax, eax%c    ret%c", 0
 
 section .text
-global main
-extern fopen, fprintf, fclose
+    global main
+    extern fopen
+    extern fclose
+    extern fprintf
 
+%macro FT 0
 main:
     push rbp
     mov rbp, rsp
-    push r12
-    push r13
-    push r14
-    
-    ; Open file
-    lea rdi, [rel filename]
-    lea rsi, [rel mode]
-    call fopen wrt ..plt
-    mov r12, rax
-    test rax, rax
-    jz exit_fail
-    
-    ; Initialize loop variables
-    mov r13, 0                    ; loop counter
-    lea r14, [rel parts_array]    ; pointer to parts array
-    
-write_loop:
-    ; Check if we've written all parts
-    cmp r13, parts_count
-    jge loop_done
-    
-    ; Get current part pointer
-    mov rsi, [r14 + r13*8]        ; Load parts_array[r13]
-    mov rdi, r12                  ; FILE pointer
-    
-    ; Handle different parts with different arguments
-    cmp r13, 0                    ; part1: %define FILENAME
-    je write_part1
-    cmp r13, 1                    ; part2: %define FILEMODE  
-    je write_part2
-    cmp r13, 2                    ; part3: %define CONTENT
-    je write_part3
-    cmp r13, 7                    ; part8: has extra newline
-    je write_part8
-    cmp r13, 10                   ; part11: has extra newline
-    je write_part11
-    cmp r13, 12                   ; part13: has 3 newlines
-    je write_part13
-    cmp r13, 13                   ; part14: has 2 newlines
-    je write_part14
-    cmp r13, 14                   ; part15: has 2 newlines
-    je write_part15
-    cmp r13, 16                   ; part17: has 2 newlines
-    je write_part17
-    cmp r13, 17                   ; part18: has 4 newlines
-    je write_part18
-    
-    ; Default case: simple parts with just one newline
-    mov rdx, 10                   ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-    
-write_part1:
-    mov rdx, 37                   ; %
-    mov rcx, 34                   ; "
-    lea r8, [rel filename]        ; filename string
-    mov r9, 34                    ; "
-    push 10                       ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    add rsp, 8
-    jmp next_part
-    
-write_part2:
-    mov rdx, 37                   ; %
-    mov rcx, 34                   ; "
-    lea r8, [rel mode]            ; mode string  
-    mov r9, 34                    ; "
-    push 10                       ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    add rsp, 8
-    jmp next_part
-    
-write_part3:
-    mov rdx, 37                   ; %
-    mov rcx, 34                   ; "
-    lea r8, [rel data]            ; content string
-    mov r9, 34                    ; "
-    push 10                       ; newline
-    push 10                       ; blank line
-    mov rax, 0
-    call fprintf wrt ..plt
-    add rsp, 16
-    jmp next_part
+    push rbx
 
-write_part8:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; blank line
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
+    open_file:
+        lea rdi, [rel filename]
+        lea rsi, [rel mode_w]
+        call fopen wrt ..plt
 
-write_part11:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; blank line
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-
-write_part13:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; newline
-    mov r8, 10                    ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-
-write_part14:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-
-write_part15:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-
-write_part17:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
-
-write_part18:
-    mov rdx, 10                   ; newline
-    mov rcx, 10                   ; newline
-    mov r8, 10                    ; newline
-    mov r9, 10                    ; newline
-    mov rax, 0
-    call fprintf wrt ..plt
-    jmp next_part
+        test rax, rax
+        jz .open_failed
+        mov rbx, rax
+        jmp write_in_file
     
-next_part:
-    inc r13                       ; increment counter
-    jmp write_loop
-    
-loop_done:
-    ; Close file
-    mov rdi, r12
-    call fclose wrt ..plt
-    
-    pop r14
-    pop r13
-    pop r12
-    pop rbp
-    xor eax, eax
-    ret
+    .open_failed:
+        mov eax, 1
+        jmp exit_process
 
-exit_fail:
-    pop r14
-    pop r13
-    pop r12
-    pop rbp
-    mov eax, 1
-    ret
+    write_in_file:
+        mov rdi, rbx
+        lea rsi, [rel model]
+        mov rdx, 10
+        mov rcx, 34
+        lea r8, [rel model]
+        mov r9, 37
+        xor rax, rax
+        call fprintf wrt ..plt
+
+    close_file:
+        mov rdi, rbx
+        call fclose wrt ..plt
+    
+    exit_process:
+        pop rbx
+        pop rbp
+        xor eax, eax
+        ret
+%endmacro
+
+FT
