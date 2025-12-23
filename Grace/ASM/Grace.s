0adf1e3d9df7331a0c284e424461265af426b34f
Jorge Verdu ruiz <jverdu-r@c2r4s1.42madrid.com>
2025-12-23 18:48:12 +0100

    all working on 42 campus computers, DONE

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -2,7 +2,7 @@
 BITS 64
 
 section .data
-    model db "; One Comment%1$cBITS 64%1$c%1$csection .data%1$c    model db %2$c%3$s%2$c, 0%1$c    filename db %2$cGrace_kid.s%2$c, 0%1$c    mode_w db %2$cw%2$c, 0%1$c%1$c%1$csection .text%1$c    global main%1$c    extern fopen%1$c    extern fclose%1$c    extern fprintf%1$c%1$c%4$cmacro FT 0%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    push rbx%1$c%1$c    open_file:%1$c        lea rdi, [rel filename]%1$c        lea rsi, [rel mode_w]%1$c        call fopen wrt ..plt%1$c%1$c        test rax, rax%1$c        jz .open_failed%1$c        mov rbx, rax%1$c        jmp write_in_file%1$c    %1$c    .open_failed:%1$c        mov eax, 1%1$c        jmp exit_process%1$c%1$c    write_in_file:%1$c        mov rdi, rbx%1$c        lea rsi, [rel model]%1$c        mov rdx, 10%1$c        mov rcx, 34%1$c        lea r8, [rel model]%1$c        mov r9, 37%1$c        xor rax, rax%1$c        call fprintf wrt ..plt%1$c%1$c    close_file:%1$c        mov rdi, rbx%1$c        call fclose wrt ..plt%1$c    %1$c    exit_process:%1$c        pop rbx%1$c        pop rbp%1$c        xor eax, eax%1$c        ret%1$c%4$cendmacro%1$c%1$cFT%1$c", 0
+model db "; One Comment%1$cBITS 64%1$c%1$csection .data%1$c    model db %2$c%3$s%2$c, 0%1$c    filename db %2$cGrace_kid.s%2$c, 0%1$c    mode_w db %2$cw%2$c, 0%1$c%1$c%1$csection .text%1$c    global main%1$c    extern fopen%1$c    extern fclose%1$c    extern fprintf%1$c%1$c%4$cmacro FT 0%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    push rbx%1$c    sub rsp, 8%1$c%1$c    open_file:%1$c        lea rdi, [rel filename]%1$c        lea rsi, [rel mode_w]%1$c        call fopen wrt ..plt%1$c%1$c        test rax, rax%1$c        jz .open_failed%1$c        mov rbx, rax%1$c        jmp write_in_file%1$c    %1$c    .open_failed:%1$c        mov eax, 1%1$c        jmp exit_process%1$c%1$c    write_in_file:%1$c        mov rdi, rbx%1$c        lea rsi, [rel model]%1$c        mov rdx, 10%1$c        mov rcx, 34%1$c        lea r8, [rel model]%1$c        mov r9, 37%1$c        xor rax, rax%1$c        call fprintf wrt ..plt%1$c%1$c    close_file:%1$c        mov rdi, rbx%1$c        call fclose wrt ..plt%1$c    %1$c    exit_process:%1$c        add rsp, 8%1$c        pop rbx%1$c        pop rbp%1$c        xor eax, eax%1$c        ret%1$c%4$cendmacro%1$c%1$cFT%1$c", 0
     filename db "Grace_kid.s", 0
     mode_w db "w", 0
 
@@ -18,6 +18,7 @@ main:
     push rbp
     mov rbp, rsp
     push rbx
+    sub rsp, 8
 
     open_file:
         lea rdi, [rel filename]
@@ -48,6 +49,7 @@ main:
         call fclose wrt ..plt
     
     exit_process:
+        add rsp, 8
         pop rbx
         pop rbp
         xor eax, eax
--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -26,7 +26,7 @@ int main(void)
     FILE *f = fopen(input, "w");
     if (!f)
         return 1;
-    char *model = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c#include <sys/types.h>%1$c#include <string.h>%1$c#include <sys/wait.h>%1$c%1$c#define NEWLINE 10%1$c#define DQUOTE 34%1$c%1$cint compile(char *input, char *output);%1$cint run(char *filename);%1$c%1$cint main(void)%1$c{%1$c    char input[] = %2$cSully_X.c%2$c;%1$c    char output[] = %2$cSully_X%2$c;%1$c    int x = %4$d;%1$c%1$c    if (strstr(__FILE__, %2$cSully.c%2$c) == NULL)%1$c        x--;%1$c    input[6] = output[6] = '0' + x;%1$c%1$c    if (x < 0)%1$c        return 0;%1$c    FILE *f = fopen(input, %2$cw%2$c);%1$c    if (!f)%1$c        return 1;%1$c    char *model = %2$c%3$s%2$c;%1$c    fprintf(f, model, NEWLINE, DQUOTE, model, x);%1$c    fclose(f);%1$c    if (compile(input, output) != EXIT_SUCCESS)%1$c        return 2;%1$c    return run(output);%1$c}%1$c%1$cint compile(char *input, char *output)%1$c{%1$c    char compiler_path[] = %2$c/usr/bin/cc%2$c;%1$c    if (!input)%1$c        return EXIT_FAILURE;%1$c%1$c    pid_t pid = fork();%1$c    if (pid < 0)%1$c        return EXIT_FAILURE;%1$c    if (pid == 0)%1$c        return execlp(compiler_path, compiler_path, input, %2$c-o%2$c, output, NULL);%1$c    int status;%1$c    waitpid(pid, &status, 0);%1$c    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)%1$c        return EXIT_SUCCESS;%1$c    return EXIT_FAILURE;%1$c}%1$c%1$cint run(char *filename)%1$c{%1$c    char path[10] = {'.', '/', 0};%1$c    strlcat(path, filename, 10);%1$c    return execlp(path, path, NULL);%1$c}";
+    char *model = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c#include <sys/types.h>%1$c#include <string.h>%1$c#include <sys/wait.h>%1$c%1$c#define NEWLINE 10%1$c#define DQUOTE 34%1$c%1$cint compile(char *input, char *output);%1$cint run(char *filename);%1$c%1$cint main(void)%1$c{%1$c    char input[] = %2$cSully_X.c%2$c;%1$c    char output[] = %2$cSully_X%2$c;%1$c    int x = %4$d;%1$c%1$c    if (strstr(__FILE__, %2$cSully.c%2$c) == NULL)%1$c        x--;%1$c    input[6] = output[6] = '0' + x;%1$c%1$c    if (x < 0)%1$c        return 0;%1$c    FILE *f = fopen(input, %2$cw%2$c);%1$c    if (!f)%1$c        return 1;%1$c    char *model = %2$c%3$s%2$c;%1$c    fprintf(f, model, NEWLINE, DQUOTE, model, x);%1$c    fclose(f);%1$c    if (compile(input, output) != EXIT_SUCCESS)%1$c        return 2;%1$c    return run(output);%1$c}%1$c%1$cint compile(char *input, char *output)%1$c{%1$c    char compiler_path[] = %2$c/usr/bin/cc%2$c;%1$c    if (!input)%1$c        return EXIT_FAILURE;%1$c%1$c    pid_t pid = fork();%1$c    if (pid < 0)%1$c        return EXIT_FAILURE;%1$c    if (pid == 0)%1$c        return execlp(compiler_path, compiler_path, input, %2$c-o%2$c, output, NULL);%1$c    int status;%1$c    waitpid(pid, &status, 0);%1$c    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)%1$c        return EXIT_SUCCESS;%1$c    return EXIT_FAILURE;%1$c}%1$c%1$cint run(char *filename)%1$c{%1$c    pid_t pid;%1$c    int status;%1$c    char path[256];%1$c    if (!filename)%1$c        return EXIT_FAILURE;%1$c    snprintf(path, sizeof(path), %2$c./%%s%2$c, filename);%1$c    pid = fork();%1$c    if (pid < 0)%1$c        return EXIT_FAILURE;%1$c    if (pid == 0) {%1$c        execlp(path, path, NULL);%1$c        _exit(127);%1$c    }%1$c    waitpid(pid, &status, 0);%1$c    if (WIFEXITED(status))%1$c        return WEXITSTATUS(status);%1$c    return EXIT_FAILURE;%1$c}%1$c";
     fprintf(f, model, NEWLINE, DQUOTE, model, x);
     fclose(f);
     if (compile(input, output) != EXIT_SUCCESS)
@@ -54,7 +54,21 @@ int compile(char *input, char *output)
 
 int run(char *filename)
 {
-    char path[10] = {'.', '/', 0};
-    strlcat(path, filename, 10);
-    return execlp(path, path, NULL);
+    pid_t pid;
+    int status;
+    char path[256];
+    if (!filename)
+        return EXIT_FAILURE;
+    snprintf(path, sizeof(path), "./%s", filename);
+    pid = fork();
+    if (pid < 0)
+        return EXIT_FAILURE;
+    if (pid == 0) {
+        execlp(path, path, NULL);
+        _exit(127); /* exec failed */
+    }
+    waitpid(pid, &status, 0);
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    return EXIT_FAILURE;
 }
